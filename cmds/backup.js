7c665d847b2385ac43b7cc37ac3212ca6b8603ed
Booteille <sephir@cryptolab.net>
2017-06-01 15:33:24 +0200

    Add examples to the help

--- cmds/backup.js
+++ cmds/backup.js
@@ -1,10 +1,21 @@
 'use strict'
 
+const chalk = require('chalk')
+
+const expl = chalk.cyan
+const desc = chalk.yellow
+
 exports.command = 'backup <command>'
 exports.desc = 'Manage Backups'
 
 exports.builder = function (yargs) {
-  return yargs.commandDir('backup')
+  return yargs
+    .commandDir('backup')
+    .example(expl('blynkcli backup create first'), desc('Create backup named "first"'))
+    .example(expl('blynkcli backup remove first'), desc('Remove backup named "first" if it is the only one with that name'))
+    .example(expl('blynkcli backup remove /0f25'), desc('Remove backup with uuid starting with "0f25"'))
+    .example(expl('blynkcli backup remove first/0f25'), desc('Remove backup named "first" and with uuid starting with "0f25"'))
+    .example(expl('blynkcli backup restore first'), desc('Restore from backup named "first"'))
 }
 
 exports.handler = function (argv) {}
--- cmds/user.js
+++ cmds/user.js
@@ -1,10 +1,19 @@
 'use strict'
 
+const chalk = require('chalk')
+
+const expl = chalk.cyan
+const desc = chalk.yellow
+
 exports.command = 'user <command>'
 exports.desc = 'Manage Blynk server users'
 
 exports.builder = function (yargs) {
-  return yargs.commandDir('user')
+  return yargs
+    .commandDir('user')
+    .example(expl('blynkcli user clone-profile user1@blynk.cc user2@blynk.cc'), desc('Copy profile (every projects) of user1@blynk.cc to user2@blynk.cc'))
+    .example(expl('blynkcli user get admin@blynk.cc energy'), desc('Display energy ammount of admin@blynk.cc'))
+    .example(expl('blynkcli user set admin@blynk.cc energy 100'), desc('Change energy ammount of admin@blynk.cc to 100'))
 }
 
 exports.handler = function (argv) {}
