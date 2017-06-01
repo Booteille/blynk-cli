19f7ef070061360af2d1cdd84cfd7dec0d376742
Booteille <sephir@cryptolab.net>
2017-06-01 16:29:53 +0200

    Renamed clone-profile to clone-projects

--- cmds/user/clone-projects.js
+++ cmds/user/clone-projects.js
@@ -1,7 +1,7 @@
 'use strict'
 
-exports.command = 'clone-profile <emailSource> <emailTarget>'
-exports.desc = 'Clone user <emailSource> profile to <emailTarget> '
+exports.command = 'clone-projects <emailSource> <emailTarget>'
+exports.desc = 'Clone user <emailSource> projects to <emailTarget> '
 exports.aliases = 'cp'
 
 exports.builder = {}
